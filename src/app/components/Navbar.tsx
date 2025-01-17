3e662b4e00ac5b8549aefdc86100f9232ec12c23
Riyan <thevaluables04@gmail.com>
2025-01-17 15:02:22 +0530

    made navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,27 +1,38 @@
 import Link from "next/link"
 import { FaLinkedin, FaInstagram } from "react-icons/fa";
+import { Button } from "@/components/ui/button"
+
 
 
 const Navbar = () => {
   return (
     <div className="flex justify-between">
-        <div className=" pl-2 flex justify-between gap-4">
-            <Link href={"#"}>
-                About Us
-            </Link>
-            <Link href={"#"}>
-                Projects
-            </Link>
-            <Link href={"#"}>
-                Services
-            </Link>
-            <Link href={"#"}>
-                Contact Us
-            </Link>
+        <div className=" pl-6 flex justify-between gap-4">
+            <Button>
+                <Link href={"#"}>
+                    About Us
+                </Link>
+            </Button>
+            <Button>
+
+                <Link href={"#"}>
+                    Projects
+                </Link>
+            </Button>
+            <Button>
+                <Link href={"#"}>
+                    Services
+                </Link>
+            </Button>
+            <Button variant={"outline"}>
+                <Link href={"#"}>
+                    Contact Us
+                </Link>
+            </Button>
         </div>
-        <div className=" flex mt-1">
-            <FaLinkedin />
-            <FaInstagram />
+        <div className=" flex mt-1 mr-2 gap-3">
+            <FaLinkedin className=" size-6" />
+            <FaInstagram className=" size-6" />
         </div>
     </div>
   )
