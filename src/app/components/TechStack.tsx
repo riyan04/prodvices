986555ef28f792e74604270b64ed6a40be7d32ca
Riyan <thevaluables04@gmail.com>
2025-01-20 22:44:15 +0530

    fixed the carousal

--- src/app/components/TechStack.tsx
+++ src/app/components/TechStack.tsx
@@ -1,56 +1,73 @@
 'use client';
 
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import { Autoplay } from "swiper/modules";
-import "swiper/css/autoplay";
+import Autoplay from "embla-carousel-autoplay"
+
+
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  // CarouselNext,
+  // CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image";
 
 type TechStack = {
-  name: string;
-  icon: string;
+  // name: string;
+  // icon: string;
+  src: string
 };
 
 const techStack: TechStack[] = [
-  { name: "JavaScript", icon: "🟨" },
-  { name: "React", icon: "⚛️" },
-  { name: "Node.js", icon: "🟩" },
-  { name: "NestJS", icon: "🔺" },
-  { name: "Docker", icon: "🐳" },
-  { name: "PostgreSQL", icon: "🐘" },
-  { name: "MongoDB", icon: "🍃" },
-  { name: "TypeORM", icon: "🛠️" },
+  // { name: "JavaScript", icon: "🟨" },
+  // { name: "React", icon: "⚛️" },
+  // { name: "Node.js", icon: "🟩" },
+  // { name: "NestJS", icon: "🔺" },
+  // { name: "Docker", icon: "🐳" },
+  // { name: "PostgreSQL", icon: "🐘" },
+  // { name: "MongoDB", icon: "🍃" },
+  // { name: "TypeORM", icon: "🛠️" },
+  {src: "logo-javascript.svg"},
+  {src: "prisma-2.svg"},
+  {src: "docker-3.svg"},
+  {src: "mongodb-icon-2.svg"},
+  {src: "nest.svg"},
 ];
 
-const TechStackCarousel: React.FC = () => {
+
+const TechStackCarousel = () => {
   return (
-    <div className="w-11/12 max-w-6xl mx-auto text-center mb-5 mt-5">
-      <h2 className="text-3xl font-bold mb-6">My Tech Stack</h2>
-      <Swiper
-        modules={[Autoplay]}
-        spaceBetween={20}
-        autoplay={{ delay: 2000, disableOnInteraction: false }}
-        loop={true}
-        breakpoints={{
-          320: { slidesPerView: 1 },
-          480: { slidesPerView: 2 },
-          768: { slidesPerView: 3 },
-          1024: { slidesPerView: 4 },
-          1440: { slidesPerView: 5 },
-        }}
-      >
-        {techStack.map((tech) => (
-          <SwiperSlide
-            key={tech.name}
-            className="flex flex-col items-center justify-center  border  rounded-full shadow-md mb-5"
-          >
-            <div className="text-3xl md:text-6xl mb-4">{tech.icon}</div>
-            <p className="font-semibold text-lg md:text-xl mb-4">{tech.name}</p>
-          </SwiperSlide>
+    
+    <Carousel
+      opts={{
+        loop: true
+      }}
+     plugins={[
+      Autoplay({
+        delay: 2000,
+      }),
+    ]} className="w-full">
+      <CarouselContent className="ml-1">
+        {techStack.map((tech, index) => (
+          <CarouselItem key={index} className="pl-1 my-auto sm:basis-1/2 md:basis-1/3 lg:basis-1/4">
+            <div className=" p-5  ">
+              <Card className=" border-none shadow-none lg:w-40">
+                <CardContent className="flex items-center justify-center p-6">
+                      <Image className=" shadow-md" src={tech.src} alt="tech" width={70} height={70} />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
         ))}
-      </Swiper>
-    </div>
+      </CarouselContent>
+      {/* <CarouselPrevious /> */}
+      {/* <CarouselNext /> */}
+    </Carousel>
+
   );
 };
 
-export default TechStackCarousel;
+export default TechStackCarousel
